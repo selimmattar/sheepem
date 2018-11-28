188d6d372344617e944f6c6fca485c75f9a15cf1
selimmattar <selim.mattar@esprit.tn>
2018-11-28 01:22:16 +0100

    :|

---
+++ Assets/DogMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMovement : MonoBehaviour {
+    public float moveSpeed = 5f;
+    public float turnSpeed = 80f;
+    // Use this for initialization
+    void Start () {
+     
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Z))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Q))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+    
+}
